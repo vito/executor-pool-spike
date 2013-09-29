702f37fd9e8ae24f4db9000a59603d9027c67a30
Alex Suraci <asuraci@pivotallabs.com>
2013-09-29 16:44:50 -0700

    starter: pass by value
    Made this by-reference because it wouldn't properly close the pipelines
    before. Found the cause: a by-value mutex passed to a goroutine keeps its
    "locked" state, and is not unlocked by the parent.
    
    Fixed by using more granular locking, not surrounding the creation of the
    goroutine.

--- starter/starter.go
+++ starter/starter.go
@@ -18,35 +18,62 @@ type Starter struct {
 	sync.RWMutex
 }
 
-func NewStarter(node executor.Node) *Starter {
-	return &Starter{
+func NewStarter(node executor.Node) Starter {
+	return Starter{
 		node: node,
 
 		pipelines: make(map[string]chan executor.Instance),
 	}
 }
 
-func (s *Starter) Start(app string, index int) {
+func (s Starter) Start(app string, index int) {
 	s.pipelineFor(app) <- executor.Instance{app, index, false}
 }
 
-func (s *Starter) pipelineFor(app string) chan executor.Instance {
-	s.Lock()
-	defer s.Unlock()
-
-	pipeline, found := s.pipelines[app]
+func (s Starter) pipelineFor(app string) chan executor.Instance {
+	pipeline, found := s.getPipe(app)
 
 	if !found {
-		pipeline = make(chan executor.Instance)
-		s.pipelines[app] = pipeline
-
+		pipeline = s.createPipe(app)
 		go s.dispatchStarts(app, pipeline)
 	}
 
 	return pipeline
 }
 
-func (s *Starter) closePipe(app string) {
+func (s Starter) dispatchStarts(app string, pipeline chan executor.Instance) {
+	for {
+		select {
+		case instanceToStart := <-pipeline:
+			fmt.Println(ansi.Color("dispatching start", "cyan"), app, instanceToStart.Index)
+			s.node.StartInstance(instanceToStart)
+
+		case <-time.After(5 * time.Second):
+			fmt.Println(ansi.Color("pipeline expired", "red"), app)
+			s.closePipe(app)
+			return
+		}
+	}
+}
+
+func (s Starter) getPipe(app string) (chan executor.Instance, bool) {
+	s.RLock()
+	defer s.RUnlock()
+
+	pipeline, found := s.pipelines[app]
+	return pipeline, found
+}
+
+func (s Starter) createPipe(app string) chan executor.Instance {
+	s.Lock()
+	defer s.Unlock()
+
+	pipeline := make(chan executor.Instance)
+	s.pipelines[app] = pipeline
+	return pipeline
+}
+
+func (s Starter) closePipe(app string) {
 	s.Lock()
 	defer s.Unlock()
 
@@ -57,19 +84,4 @@ func (s *Starter) closePipe(app string) {
 
 	close(pipeline)
 	delete(s.pipelines, app)
-}
-
-func (s *Starter) dispatchStarts(app string, pipeline chan executor.Instance) {
-	for {
-		select {
-		case instanceToStart := <-pipeline:
-			fmt.Println(ansi.Color("dispatching start", "cyan"), app, instanceToStart.Index)
-			s.node.StartInstance(instanceToStart)
-
-		case <-time.After(10 * time.Second):
-			fmt.Println(ansi.Color("pipeline expired", "red"), app)
-			s.closePipe(app)
-			return
-		}
-	}
 }
